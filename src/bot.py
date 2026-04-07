e9721426ec45edccca63ca753884fdd37976d73f
Rehan Ahmed <rehansalmani2005@gmail.com>
2026-04-07 12:25:26 +0530

    Button handler Fucntions

--- src/bot.py
+++ src/bot.py
@@ -7,7 +7,7 @@ load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, CallbackQueryHandler, ContextTypes, CommandHandler
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -27,10 +27,20 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reply_markup = reply_markup
 )
 
+async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+    if query.data == 'expense':
+        await query.edit_message_text(text="Where are we Burning the money, sir")
+    elif query.data == 'income':
+        await query.edit_message_text(text="Money Laundering, sir?")
+
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
 
     start_handler = CommandHandler('start', start)
+    application.add_handler(CallbackQueryHandler(button_handler))
     application.add_handler(start_handler)
 
     application.run_polling()
