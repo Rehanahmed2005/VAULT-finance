ec3d05b1d3addee7a74eacc3afc7331c0431e750
Rehan Ahmed <rehansalmani2005@gmail.com>
2026-04-12 18:37:48 +0530

    Added Note as an Parameter

--- src/models.py
+++ src/models.py
@@ -1,10 +1,11 @@
 from datetime import datetime 
 
 class Transaction:
-    def __init__(self, trans_type, amount, category, date=None):
+    def __init__(self, trans_type, amount, category, note, date=None):
         self.trans_type = trans_type
         self.amount = amount
         self.category = category
+        self.note = note
         self.date = date if date else datetime.now().strftime("%Y-%m-%d")
 
     def to_dict(self):
@@ -12,6 +13,7 @@ class Transaction:
             "trans_type": self.trans_type,
             "amount": self.amount,
             "category": self.category,
+            "note":self.note,
             "date": self.date
         }
     
@@ -21,5 +23,6 @@ class Transaction:
             trans_type=data["trans_type"],
             amount=data["amount"],
             category=data["category"],
+            note=data["note"],
             date=data.get("date")
         )
